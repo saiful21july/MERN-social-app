06fa12c74151bec76f5244f7613d49254d844318
saiful <saiful21july@gmail.com>
2021-06-23 23:33:06 +0300

    first commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,5 +130,139 @@ router.get('/user/:user_id', async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+// @route DELETE api/profile
+// @desc  Delete profile, user and posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id }) // this is private so we have access to the token which actually we have to add auth middleware.
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id })
+    res.json({ msg: 'User deleted' })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+// @route PUT api/profile/experience
+// @desc  Add profile experience
+// @access Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { title, company, location, from, to, current, description } =
+      req.body
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    }
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      profile.experience.unshift(newExp)
+      await profile.save()
+      res.json(profile)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+// @route DELETE api/profile/experience/:exp_id
+// @desc DELETE experience from profile
+//@access private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    // GET remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id)
+    profile.experience.splice(removeIndex, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+// @route PUT api/profile/education
+// @desc  Add profile education
+// @access Private
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { school, degree, fieldofstudy, from, to, current, description } =
+      req.body
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    }
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      profile.education.unshift(newEdu)
+      await profile.save()
+      res.json(profile)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+// @route DELETE api/profile/education/:edu_id
+// @desc DELETE education from profile
+//@access private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    // GET remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id)
+    profile.education.splice(removeIndex, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
 // Now let's reiterate what's going on here on this file is that we check for the errors, body errors and pull everything from the body and then we build up this profileFields object to insert into database and we need to check to see if the stuffs actually coming in before we set it and then we need to initialize social object and do the same for profile.social like profile.user.Then we are going to go ahead look for profile by the user if it is found then we will update it and send back the profile, if it is not found we are just going to create it, save it and send back the profile
