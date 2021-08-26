7ab6c366ddf9e28a9f183d2fe4317e95447e3cc0
saiful <saiful21july@gmail.com>
2021-08-26 12:53:42 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import { Fragment } from 'react'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Alert from './components/layout/Alert'
+// bring the Redux here
+import { Provider } from 'react-redux'
+import store from './store'
 
-const App = () => {
-  return (
-    <Fragment>
-      <h1>App</h1>
-    </Fragment>
-  )
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          {/*I would like to have section here with the className of container because every page within the theme except for the landing page has a class of container to push everything to the middle */}
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+)
 
 export default App
